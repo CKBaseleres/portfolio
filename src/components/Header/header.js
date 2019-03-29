a3e793d1a60c6b166d313729329f2674beb83225
Carl Keneth <baseleres.ck@gmail.com>
2019-03-29 17:18:48 +0800

    added 'test'

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,7 +8,7 @@ class Header extends Component {
 				<div className="row banner">
 					<div className="banner-text">
 						<h1 className="responsive-headline">Hi! I'm {resumeData.name}.</h1>
-						<h3>I am a {resumeData.role}.<br/>
+						<h3>I am a {resumeData.role}. test<br/>
 						{resumeData.roleDescription}
 						</h3>
 						<hr/>
