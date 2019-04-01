66f429a5dee03115da6cb6310df3bd764c0a7d1f
Carl Keneth <baseleres.ck@gmail.com>
2019-04-01 18:17:54 +0800

    removed navbar section

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,21 +5,6 @@ class Header extends Component {
 		let resumeData = this.props.resumeData;
 		return (
 			<header id="home">
-				<nav id="nav-wrap" style="display: block;" className="opaque">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className=""><a className="smoothscroll" href="#home">Home</a></li>
-            <li className=""><a className="smoothscroll" href="#about">About</a></li>
-	         <li className="current"><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li className=""><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li className=""><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li className=""><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
-      </nav>
 				<div className="row banner">
 					<div className="banner-text">
 						<h1 className="responsive-headline">Hi! I'm {resumeData.name}.</h1>
