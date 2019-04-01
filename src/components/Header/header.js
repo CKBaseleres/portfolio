2d7e1ed699518b4117467e8b47d3c7e6dbe8008d
Carl Keneth <baseleres.ck@gmail.com>
2019-04-01 18:09:42 +0800

    Reverted to previous version without nav

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,23 +5,6 @@ class Header extends Component {
 		let resumeData = this.props.resumeData;
 		return (
 			<header id="home">
-
-				<nav id="nav-wrap" style="display: block;">
-				
-	        <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-		      <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" class="nav">
-            <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
-            <li><a class="smoothscroll" href="#about">About</a></li>
-	         <li><a class="smoothscroll" href="#resume">Resume</a></li>
-            <li><a class="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a class="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
-      	</nav>
-
 				<div className="row banner">
 					<div className="banner-text">
 						<h1 className="responsive-headline">Hi! I'm {resumeData.name}.</h1>
