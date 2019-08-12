740d78fce6d1ceba0f6cc869f860768cd041a1c0
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2019-08-12 11:28:02 +0800

    Added more target props to links

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-	render(){
-		return(
-			<footer>
-				<div className="row">
-					<div className='twelve columns'>
-						<ul className='social-links'>
-							<li>
-								<a href="https://www.linkedin.com/in/carl-keneth-baseleres-782642131">
-									<i className="fa fa-linkedin"></i>
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/CKBaseleres">
-									<i className="fa fa-github"></i>
-								</a>
-							</li>
-							<li>
+  render() {
+    return (
+      <footer>
+        <div className='row'>
+          <div className='twelve columns'>
+            <ul className='social-links'>
+              <li>
+                <a
+                  href='https://www.linkedin.com/in/carl-keneth-baseleres-782642131'
+                  target='blank'>
+                  <i className='fa fa-linkedin' />
+                </a>
+              </li>
+              <li>
+                <a href='https://github.com/CKBaseleres' target='blank'>
+                  <i className='fa fa-github' />
+                </a>
+              </li>
+              {/* <li>
 								<a href="https://www.facebook.com/">
 									<i className="fa fa-facebook"></i>
 								</a>
-							</li>
-						</ul>
-						<ul className='copyright'>
-							© Copyright 2019 Carl Keneth Baseleres
-						</ul>
-					</div>
-				</div>
-				<div id="go-top">
-					<a className="smoothscroll" title="Back to Top" href="#home">
-						<i className='icon-up-open'></i>
-					</a>
-				</div>
-			</footer>
-		)
-	}
+							</li> */}
+            </ul>
+            <ul className='copyright'>
+              © Copyright 2019 Carl Keneth Baseleres
+            </ul>
+          </div>
+        </div>
+        <div id='go-top'>
+          <a className='smoothscroll' title='Back to Top' href='#home'>
+            <i className='icon-up-open' />
+          </a>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,39 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
-	render() {
-		let resumeData = this.props.resumeData;
-		return (
-			<header id="home">
-				<div className="row banner">
-					<div className="banner-text">
-						<h1 className="responsive-headline">Hi! I'm {resumeData.name}.</h1>
-						<h3>I am a {resumeData.role}.<br/>
-						{resumeData.roleDescription}
-						</h3>
-						<hr/>
-						<ul className="social">
-							<li>
-								<a href="https://www.linkedin.com/in/carl-keneth-baseleres-782642131">
-									<i className="fa fa-linkedin"></i>
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/CKBaseleres">
-									<i className="fa fa-github"></i>
-								</a>
-							</li>
-							<li>
-								<a href="https://www.facebook.com/">
-									<i className="fa fa-facebook"></i>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</header>
-		)
-	}
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <header id='home'>
+        <div className='row banner'>
+          <div className='banner-text'>
+            <h1 className='responsive-headline'>Hi! I'm {resumeData.name}.</h1>
+            <h3>
+              I am a {resumeData.role}.<br />
+              {resumeData.roleDescription}
+            </h3>
+            <hr />
+            <ul className='social'>
+              <li>
+                <a
+                  href='https://www.linkedin.com/in/carl-keneth-baseleres-782642131'
+                  target='blank'>
+                  <i className='fa fa-linkedin' />
+                </a>
+              </li>
+              <li>
+                <a href='https://github.com/CKBaseleres' target='blank'>
+                  <i className='fa fa-github' />
+                </a>
+              </li>
+              {/* <li>
+                <a href='https://www.facebook.com/' >
+                  <i className='fa fa-facebook' />
+                </a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
